91991730455c9c4c411b2119adea2108005d3b1e
BETTER <nowqwer@mail.ru>
2024-04-06 16:09:58 +0500

    first commit

---
+++ src/main/java/com/better/one_better_bot/OneBetterBotApplication.java
@@ -0,0 +1,13 @@
+package com.better.one_better_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneBetterBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneBetterBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/better/one_better_bot/OneBetterBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.better.one_better_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneBetterBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
