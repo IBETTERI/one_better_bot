2a6eb00636575e8d4ae97f24f8ee5e6053f5afb8
BETTER <nowqwer@mail.ru>
2024-04-06 16:30:09 +0500

    1337

--- src/main/java/com/better/one_better_bot/config/BotConfig.java
+++ src/main/java/com/better/one_better_bot/config/BotConfig.java
@@ -5,11 +5,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-@Configuration
-@Data
-@PropertySource("application.properties")
+@Configuration//
+@Data//
+@PropertySource("application.properties")///
 public class BotConfig {
-    @Value("${bot.name}")
+    @Value("${bot.name}")//
     String botName;
     @Value("${bot.token}")
     String token;
