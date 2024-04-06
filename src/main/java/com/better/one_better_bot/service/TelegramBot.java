8c2dd11e57c685e2a607dee41948ea88a91b553c
BETTER <nowqwer@mail.ru>
2024-04-06 20:23:22 +0500

    сделал меню

--- src/main/java/com/better/one_better_bot/service/TelegramBot.java
+++ src/main/java/com/better/one_better_bot/service/TelegramBot.java
@@ -1,19 +1,41 @@
 package com.better.one_better_bot.service;
 
+
 import com.better.one_better_bot.config.BotConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
+
     final BotConfig config;
+
     public TelegramBot(BotConfig config) {
         this.config = config;
+
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "get a welcom message"));
+        listOfCommands.add(new BotCommand("/mydata","get you data stored"));
+        listOfCommands.add(new BotCommand("/deletedata", "delete my data"));
+        listOfCommands.add(new BotCommand("/settings", "set you preferences settings"));
+        listOfCommands.add(new BotCommand("/help", "info how to use this command"));
+        try{
+            this.execute(new SetMyCommands(listOfCommands,new BotCommandScopeDefault(),null));
+        }
+        catch (TelegramApiException e) {
+            log.error("Error setting bot command list", e.getMessage());
+        }
     }
     @Override
     public String getBotUsername() {
@@ -32,6 +54,8 @@ public class TelegramBot extends TelegramLongPollingBot {
                 case "/start":
                         startCommandReceived(chatId,update.getMessage().getChat().getFirstName());
                     break;
+                    case "/mydata":
+
                 default: sendMessage(chatId,"Sorry, command not found");
             }
         }
