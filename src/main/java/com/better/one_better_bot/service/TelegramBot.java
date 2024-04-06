e9e6f074c70a9c201d8924112c4f05bb619b0f85
BETTER <nowqwer@mail.ru>
2024-04-06 19:48:50 +0500

    1

--- src/main/java/com/better/one_better_bot/service/TelegramBot.java
+++ src/main/java/com/better/one_better_bot/service/TelegramBot.java
@@ -1,12 +1,14 @@
 package com.better.one_better_bot.service;
 
 import com.better.one_better_bot.config.BotConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+@Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
     final BotConfig config;
@@ -36,6 +38,9 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
     private void startCommandReceived(long chatId, String name) {
         String answer = "Hi, " + name + " ,nice to meet you!";
+        log.info("Replied to user " + name);
+
+
         sendMessage(chatId,answer);
     }
     private void sendMessage(long chatId, String textToSend){
@@ -46,7 +51,7 @@ public class TelegramBot extends TelegramLongPollingBot {
             execute(message);
         }
         catch (TelegramApiException e) {
-            throw new RuntimeException(e);
+            log.error("Error occurred: " + e.getMessage());
         }
     }
 }
