7a5af3e697d409087816440f355efdab98cff084
BETTER <nowqwer@mail.ru>
2024-04-07 16:44:22 +0500

    ошибка заключается то что закоментировано или в базеданых

--- src/main/java/com/better/one_better_bot/model/User.java
+++ src/main/java/com/better/one_better_bot/model/User.java
@@ -1,67 +1,67 @@
 package com.better.one_better_bot.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
 
-import java.sql.Timestamp;
-
-@Entity(name = "UserDataTable")
-public class User {
-    @Id
-    private Long chatId;
-
-    public Long getChatId() {
-        return chatId;
-    }
-    private String firstName;
-    private String lastName;
-    private String userName;
-    private Timestamp joinDate;
-
-    public void setChatId(Long chatId) {
-        this.chatId = chatId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public Timestamp getJoinDate(Timestamp timestamp) {
-        return joinDate;
-    }
-
-    public void setJoinDate(Timestamp joinDate) {
-        this.joinDate = joinDate;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "chatId=" + chatId +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", userName='" + userName + '\'' +
-                ", joinDate=" + joinDate +
-                '}';
-    }
-}
+//import java.sql.Timestamp;
+//
+//@Entity(name = "UserDataTable")
+//public class User {
+//    @Id
+//    private Long chatId;
+//
+//    public Long getChatId() {
+//        return chatId;
+//    }
+//    private String firstName;
+//    private String lastName;
+//    private String userName;
+//    private Timestamp joinDate;
+//
+//    public void setChatId(Long chatId) {
+//        this.chatId = chatId;
+//    }
+//
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    public String getUserName() {
+//        return userName;
+//    }
+//
+//    public void setUserName(String userName) {
+//        this.userName = userName;
+//    }
+//
+//    public Timestamp getJoinDate(Timestamp timestamp) {
+//        return joinDate;
+//    }
+//
+//    public void setJoinDate(Timestamp joinDate) {
+//        this.joinDate = joinDate;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "User{" +
+//                "chatId=" + chatId +
+//                ", firstName='" + firstName + '\'' +
+//                ", lastName='" + lastName + '\'' +
+//                ", userName='" + userName + '\'' +
+//                ", joinDate=" + joinDate +
+//                '}';
+//    }
+//}
--- src/main/java/com/better/one_better_bot/repository/UserRepository.java
+++ src/main/java/com/better/one_better_bot/repository/UserRepository.java
@@ -1,7 +1,7 @@
-package com.better.one_better_bot.repository;
-
-import com.better.one_better_bot.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User,Long> {
-}
+//package com.better.one_better_bot.repository;
+//
+//import com.better.one_better_bot.model.User;
+//import org.springframework.data.repository.CrudRepository;
+//
+//public interface UserRepository extends CrudRepository<User,Long> {
+//}
